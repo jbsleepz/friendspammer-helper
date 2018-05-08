667488bfa34dc12086bf0e077f055971aeb5ffd0
joukebouwe <jouke-bouwe.punter@student.hu.nl>
2018-05-08 21:12:40 +0200

    committed, test om de wrecker te laten falen.

--- src/test/java/com/example/project/testing.java
+++ src/test/java/com/example/project/testing.java
@@ -6,12 +6,8 @@ import static org.junit.Assert.fail;
 
 public class testing {
 
-    public class Testing {
-
-        @Test
-        public void test_fail() {
-            fail("Let's test the failing unit test");
-        }
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
     }
-
 }
