fab0b538b414d292c2992ba1491e2c2d8bf5720e
joukebouwe <jouke-bouwe.punter@student.hu.nl>
2018-05-08 22:27:00 +0200

    test fails the artifact build?

--- src/test/java/com/example/project/testing.java
+++ src/test/java/com/example/project/testing.java
@@ -1,13 +1,13 @@
-package com.example.project;
-
-import org.junit.Test;
-
-import static org.junit.Assert.fail;
-
-public class testing {
-
-    @Test
-    public void test_fail() {
-        fail("Let's test the failing unit test");
-    }
-}
+//package com.example.project;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.fail;
+//
+//public class testing {
+//
+//    @Test
+//    public void test_fail() {
+//        fail("Let's test the failing unit test");
+//    }
+//}
